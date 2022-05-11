4415f34eab729e0807f32bf14d2301a26f39af88
mattnorris1990 <mattnorris1990@gmail.com>
2022-05-11 21:20:06 +0100

    added more css

--- src/components/SongsList.js
+++ src/components/SongsList.js
@@ -5,9 +5,9 @@ const SongsList = ({songs}) => {
     const songItems = songs.map((song, index) => {
         return (
         <div className = "song-container">
+        <li className="position-number"><h2>Position {index +1}</h2></li>
         <ul>
             <li><img src = {song["im:image"][2].label} className = "image"/></li>
-            <li>Position: {index +1}</li>
             <li><h2>{song["im:name"].label}</h2></li>
             <li><h3>{song["im:artist"].label}</h3></li>
             <li><audio controls><source src={song.link[1].attributes.href} /></audio></li>
